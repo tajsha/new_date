008ec9d8f230e012cfb7c60aa1819c8a9543f948
shaik <shaik@mahaswami.com>
2015-03-16 12:23:13 +0530

    search take you to signup screen.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -32,46 +32,50 @@ class SearchesController < ApplicationController
   end
   
   def index
-    @search = Search.new
-    
-    if params[:gender].present? and params[:search].nil?
-        conditions = {}
-        range_cond = {}
-          conditions[:gender] = params["gender"].join('|') if params["gender"].present?
-          conditions[:ethnicity] = params["ethnicity"].join('|') if params["ethnicity"].present?
-          if params["zip_code"].present?
-			l = Location.find_by_zip_code(params["zip_code"])
-			lat = l.latitude * Math::PI / 180.0
-			lng = l.longitude * Math::PI / 180.0
-		  else
-		    lat = current_user.latitude * Math::PI / 180.0
-		    lng = current_user.longitude * Math::PI / 180.0
-		  end
-          conditions[:religion] = params["religion"].join('|') if params["religion"].present?
-          range_cond[:age] = if params["min_age"].present? and params["max_age"].present?
-									params["min_age"].to_i..params["max_age"].to_i 
-							 elsif params["min_age"].present?
-									params["min_age"].to_i..65
-							 elsif params["max_age"].present?
-									18..params["max_age"].to_i
-							 end
-		  range_cond[:geodist] = 0.0..100_000.0
-          conditions[:children] = params["children"].join('|') if params["children"].present?
-		  @users = User.search(:conditions => conditions, 
-					:with => range_cond,
-					:geo => [lat, lng],
-					:order => 'geodist ASC', :without => {:user_id => current_user.id}
-					)
-    else                    
-          params[:search] = params[:search] || ''
-           @users = User.search(params[:search].gsub(/\s+/, ' | '),
-            :geo => [current_user.latitude * Math::PI / 180.0, current_user.longitude * Math::PI / 180.0],
-            :with  => {:geodist => 0.0..100_000.0},
-            :order => 'geodist ASC', :without => {:user_id => current_user.id})
-                        
-                   
-        end      
-        render 'users/index', layout: 'new_application'    
+    if current_user.present?
+		@search = Search.new
+		
+		if params[:gender].present? and params[:search].nil?
+			conditions = {}
+			range_cond = {}
+			  conditions[:gender] = params["gender"].join('|') if params["gender"].present?
+			  conditions[:ethnicity] = params["ethnicity"].join('|') if params["ethnicity"].present?
+			  if params["zip_code"].present?
+				l = Location.find_by_zip_code(params["zip_code"])
+				lat = l.latitude * Math::PI / 180.0
+				lng = l.longitude * Math::PI / 180.0
+			  else
+				lat = current_user.latitude * Math::PI / 180.0
+				lng = current_user.longitude * Math::PI / 180.0
+			  end
+			  conditions[:religion] = params["religion"].join('|') if params["religion"].present?
+			  range_cond[:age] = if params["min_age"].present? and params["max_age"].present?
+										params["min_age"].to_i..params["max_age"].to_i 
+								 elsif params["min_age"].present?
+										params["min_age"].to_i..65
+								 elsif params["max_age"].present?
+										18..params["max_age"].to_i
+								 end
+			  range_cond[:geodist] = 0.0..100_000.0
+			  conditions[:children] = params["children"].join('|') if params["children"].present?
+			  @users = User.search(:conditions => conditions, 
+						:with => range_cond,
+						:geo => [lat, lng],
+						:order => 'geodist ASC', :without => {:user_id => current_user.id}
+						)
+		else                    
+			  params[:search] = params[:search] || ''
+			   @users = User.search(params[:search].gsub(/\s+/, ' | '),
+				:geo => [current_user.latitude * Math::PI / 180.0, current_user.longitude * Math::PI / 180.0],
+				:with  => {:geodist => 0.0..100_000.0},
+				:order => 'geodist ASC', :without => {:user_id => current_user.id})
+							
+					   
+			end      
+			render 'users/index', layout: 'new_application'    
+	  else
+		redirect_to "/signup" 
+	  end
         
       end
   def min_age
